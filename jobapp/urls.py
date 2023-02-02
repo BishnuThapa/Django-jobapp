dd9ae0eda67349060a988c79a88cf1935ebac2a2
Bishnu <thapabishnu20@gmail.com>
2023-02-02 08:05:10 +0545

    added app urls

--- jobapp/urls.py
+++ jobapp/urls.py
@@ -14,15 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
-
-
-def hello(request):
-    return HttpResponse("Hello World")
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', hello)
+    path('', include('app.urls')),
 ]
