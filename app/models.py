aa1e6193c00f9aedae8ec83a7c287b3d1df5323d
Bishnu <thapabishnu20@gmail.com>
2023-02-03 15:50:38 +0545

    added new fields to existing model

--- app/models.py
+++ app/models.py
@@ -1,8 +1,14 @@
 from django.db import models
-
+import datetime
 # Create your models here.
 
 
 class JobPost(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.TextField(max_length=255)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=255)
+    posted_date = models.DateTimeField(
+        auto_now_add=True)
+    salary = models.IntegerField()
+
+    def __str__(self):
+        return self.title
