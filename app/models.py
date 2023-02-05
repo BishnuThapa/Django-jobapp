66533d3b83873a5f6bb3e5d85abfc14edd143f0f
Bishnu <thapabishnu20@gmail.com>
2023-02-05 20:55:19 +0545

    expiry date added

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 import datetime
 # Create your models here.
 
@@ -6,9 +7,10 @@ import datetime
 class JobPost(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(null=True, max_length=100, unique=True)
-    description = models.TextField()
+    description = RichTextField()
     posted_date = models.DateTimeField(
         auto_now_add=True)
+    expiry = models.DateField(null=True)
     salary = models.IntegerField()
 
     def __str__(self):
