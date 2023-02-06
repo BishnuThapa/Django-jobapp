6314b928a664e8797cf769162be250737e0a3243
Bishnu <thapabishnu20@gmail.com>
2023-02-06 14:23:10 +0545

    subscribe model created

--- subscribe/models.py
+++ subscribe/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Subscribe(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
--- subscribe/views.py
+++ subscribe/views.py
@@ -4,5 +4,18 @@ from django.shortcuts import render
 
 
 def subscribe(request):
-    context = {}
+    email_error_empty = ""
+    # Server side validation in the views
+    if request.method == "POST":
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        email = request.POST['email']
+        if email == "":
+            email_error_empty = "No Email Entered."
+        subscribe_user = subscribe(
+            firstname=firstname, lastname=lastname, email=email)
+        subscribe_user.save()
+    context = {
+        'email_error_empty': email_error_empty,
+    }
     return render(request, 'subscribe/subscribe.html', context)
