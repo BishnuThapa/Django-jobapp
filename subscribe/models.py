08c2920d8318f07cbb3745f20480bed646a98aac
Bishnu <thapabishnu20@gmail.com>
2023-02-14 12:41:32 +0545

    validation on model

--- subscribe/models.py
+++ subscribe/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Subscribe(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
 
     def __str__(self):
         return self.email
