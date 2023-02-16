88e16d71a04b0f644797668eeb51ddbffd5817ea
Bishnu <thapabishnu20@gmail.com>
2023-02-16 21:06:43 +0545

    user input form for uploading images configured

--- uploadapp/views.py
+++ uploadapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from uploadapp.forms import UploadForm
 # Create your views here.
+
+
+def upload_image(request):
+    form = UploadForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'uploadapp/add_image.html', context)
