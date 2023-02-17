9360a7e7a2fd7c51d678f78e84234807de08e32d
Bishnu <thapabishnu20@gmail.com>
2023-02-17 20:22:04 +0545

    saving & uploading images

--- uploadapp/views.py
+++ uploadapp/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from uploadapp.forms import UploadForm
-# Create your views here.
 
 
 def upload_image(request):
-    form = UploadForm()
-    context = {
-        'form': form
-    }
+    if request.method == 'POST':
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid:
+            form.save()
+    else:
+        form = UploadForm()
+    context = {'form': form}
     return render(request, 'uploadapp/add_image.html', context)
