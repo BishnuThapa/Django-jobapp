44315f8355164de7c3862e9fa4835ef7b091ead2
Bishnu <thapabishnu20@gmail.com>
2023-02-18 22:37:50 +0545

    added required settings on settings.py file & urls.py

--- jobapp/urls.py
+++ jobapp/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
@@ -25,4 +27,4 @@ urlpatterns = [
     path('', include('subscribe.urls')),
     path('uploads/', include('uploadapp.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
